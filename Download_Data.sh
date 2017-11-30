8792b598129ae9930b6132a12d44b0d3cab07a2c
Charlotte Lopp <clopp@dons.usfca.edu>
2017-11-30 06:02:05 -0800

    Add bash script that downloads the data

---
+++ Download_Data.sh
@@ -0,0 +1,15 @@
+# Charlotte Lopp
+# clopp@dons.usfca.edu
+# November 21, 2017
+
+# Script written to run through the following analysis workflow: starting with downloading of fastq files, through QC checks, trimming, and 
+# finally BLASTing them against a local copy of GenBank
+
+export PATH=$PATH:/Users/charlottelopp/Desktop/CharlotteLopp-final-project/sratoolkit.2.8.2-1-mac64/bin
+
+# Code to download the list of fles in the run table of the raw data directory
+for SRA_number in $(cut -f 8 data/metadata/SraRunTable.txt | tail -n +2)
+do
+    fastq-dump -v $SRA_number -O data/raw_data
+done
+echo "Downloaded the list of files in the run table of the raw data directory"
